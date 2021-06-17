babb0635d06dd9d8a1732bc9b9565dbc1557dc44
Himanshu Mishra <mhimanshu712@gmail.com>
2021-06-17 20:23:28 +0530

    framework ready

--- main.js
+++ main.js
@@ -1,6 +1,41 @@
 import './style.css'
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+import firebase  from 'firebase/app';
+import 'firebase/firestore';
+import fbConfig from './secret';
+
+const firebaseConfig = fbConfig;
+
+
+if(!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+const firestore = firebase.firestore();
+
+const servers = {
+  iceServers: [
+    {
+      urls: ['stun:stun1.l.google.com:19302', 'stun:stun2.l.google.com:19302'],
+    }
+  ],
+  iceCandidatePoolSize: 10
+}
+
+let pc = new RTCPeerConnectionIceEvent(servers);
+let localStream = null;
+let remoteStream = null;
+
+
+const webcamButton = document.querySelector('#webcamButton');
+const webcamVideo = document.querySelector('#webcamVideo');
+const webcamVideo = document.querySelector('#callButton');
+const webcamVideo = document.querySelector('#callInput');
+const webcamVideo = document.querySelector('#answerButton');
+const webcamVideo = document.querySelector('#remoteVideo');
+const webcamVideo = document.querySelector('#hangupButton');
+
+
+webcamButton.onclick = async () => {
+  localStream = await navigator.mediaDevices.getUserMedia({video:true, audio:true});
+}
